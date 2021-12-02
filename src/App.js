6926bd3f6301ab203461cf0727d5736c3945f027
closerearth <closer.earth@protonmail.com>
2021-12-02 10:50:09 +0000

    hello world

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Intro from './components/Intro';
+
+function App() {
+  return (
+    <div className="App">
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.js
@@ -0,0 +1,24 @@
+import './intro.css';
+
+const Intro = (props) => (
+  <main className="intro">
+    <section>
+      <h1>Closer DAO</h1>
+      <h2>The operating system for land stewardship communities</h2>
+    </section>
+    <section>
+      <p>Hop onto our <a href="https://discord.gg/cSMRqCeS" target="_blank">Discord</a> or <a href="https://t.me/+rdZvSdohTzs0Njlh" target="_blank">Telegram</a></p>
+    </section>
+    <section>
+      <ol>
+        <li>Data sovereignty</li>
+        <li>Value distribution</li>
+        <li>Digital + physical spaces for communities</li>
+        <li>Platform owned collectively by stakeholders</li>
+        <li>Regenerative cultural practices baked into the interface</li>
+      </ol>
+    </section>
+  </main>
+);
+
+export default Intro;
