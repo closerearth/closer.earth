21848a69a7dc26c7ffe486d349b5f8683ab400ea
Closer Earth <closer.earth@protonmail.com>
2022-03-12 19:05:14 -0600

    Update styles

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/header.css';
+import Wallet from './Wallet.tsx';
+
+const Header = (props) => (
+  <header>
+    <h1>Closer</h1>
+    <Wallet />
+  </header>
+);
+
+export default Header;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,24 +1,32 @@
-import './intro.css';
+import '../styles/intro.css';
 
-const Intro = (props) => (
-  <main className="intro">
-    <section>
-      <h1>Closer DAO</h1>
-      <h2>The operating system for land stewardship communities</h2>
-    </section>
-    <section>
-      <p>Hop onto our <a href="https://discord.gg/cSMRqCeS" target="_blank">Discord</a> or <a href="https://t.me/+rdZvSdohTzs0Njlh" target="_blank">Telegram</a></p>
-    </section>
-    <section>
-      <ol>
-        <li>Data sovereignty</li>
-        <li>Value distribution</li>
-        <li>Digital + physical spaces for communities</li>
-        <li>Platform owned collectively by stakeholders</li>
-        <li>Regenerative cultural practices baked into the interface</li>
-      </ol>
-    </section>
-  </main>
-);
+const Intro = (props) => {
+  return (
+    <main className="intro">
+      <section>
+        <h1>Closer</h1>
+        <h2><i>The operating system for <br/>sovereign communities</i></h2>
+      </section>
+      <section>
+        <ol>
+          <li>Data sovereignty</li>
+          <li>Value distribution</li>
+          <li>Digital + physical spaces for communities</li>
+          <li>Platform owned collectively by stakeholders</li>
+          <li>Regenerative cultural practices baked into the interface</li>
+        </ol>
+        <p className="mt-2">
+          <a href="https://dev.closer.earth" className="btn-primary" target="_blank">
+            Try the beta{' '}
+            <img src="/images/icons/link.svg" width="22" alt="" />
+          </a>
+        </p>
+      </section>
+      <section>
+        <p>Hop onto our <a rel="noreferrer" href="https://discord.gg/cSMRqCeS" target="_blank">Discord</a> or <a rel="noreferrer" href="https://t.me/+rdZvSdohTzs0Njlh" target="_blank">Telegram</a>.</p>
+      </section>
+    </main>
+  );
+}
 
 export default Intro;
